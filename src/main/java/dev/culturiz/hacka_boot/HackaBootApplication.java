5f6b6b01d5d094e165180ab7856a0e571331404b
MeetTak <starmeettak@gmail.com>
2025-03-29 00:21:33 +0530

    first commit

---
+++ src/main/java/dev/culturiz/hacka_boot/HackaBootApplication.java
@@ -0,0 +1,13 @@
+package dev.culturiz.hacka_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/culturiz/hacka_boot/HackaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.culturiz.hacka_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
